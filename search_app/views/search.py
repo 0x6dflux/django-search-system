87f8b49aee5f23003260a0769505883399ee0e39
0x6dflux <mohammadi.mahdi.mecheng@gmail.com>
2026-06-13 15:00:04 +0330

    Modify the search page
    The below features have been developed in this commit:
    - timing the whole process of searching
    - performing the search on the database
    - transferring the results to the template

--- search_app/views/search.py
+++ search_app/views/search.py
@@ -4,6 +4,8 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views import View
+from search_app.models import FakeModel
+from time import time
 
 
 class SearchForm(forms.Form):
@@ -21,9 +23,31 @@ class SearchView(View):
         )
 
     def post(self, request: HttpRequest) -> HttpResponse:
-        results = ["test1", "test2"]
+        start_time = time()
 
-        context = {"form": SearchForm(), "results": results}
+        search_field=request.POST.get('search_field')
+        context = {'rows_count':FakeModel.objects.count()}
+
+        # guess the search_field type, name, phone, national_code
+
+        # do the search
+        results = set()
+        results.update(q1:=FakeModel.objects.filter(first_name__icontains=search_field))
+        results.update(q2:=FakeModel.objects.exclude(last_name=(i for i in q1.all())).filter(last_name__icontains=search_field))
+        results.update(q3:=FakeModel.objects.exclude(city=(j for j in q2.all())).filter(city__icontains=search_field))
+        results.update(q4:=FakeModel.objects.exclude(phone_number=(k for k in q3.all())).filter(phone_number__icontains=search_field))
+        results.update(FakeModel.objects.exclude(national_code=(l for l in q4.all())).filter(national_code__icontains=search_field))
+        
+        # searching on city will reveal all people living there !!! is it ok ??
+        
+        context["results"]= results
+        context['results_count'] = len(results)
+        context["form"]= SearchForm()
+
+        # request.POST['search_field'] ------ how to delete ??
+
+        
+        context['search_time']= time() - start_time
 
         return render(
             request,
